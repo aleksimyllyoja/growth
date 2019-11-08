7fce372b9b052cd7c2b59e3b6531a7a755af7c7c
Aleksi Myllyoja <aleksi.myllyoja@kivra.fi>
2019-11-08 22:46:56 +0200

    asd

--- js/app.js
+++ js/app.js
@@ -1,33 +1,13 @@
 var stats, scene, renderer, composer;
-var camera, cameraControls;
+var camera, cameraControls, helper;
 var geometry, mesh, material;
-
-var Variables = function(){
-  this.scale = 0.1
-  this.rarity = 0.3
-}
-var variables = new Variables();
-
-var gui = new dat.GUI();
-gui.add(variables, 'scale', 0, 2, 0.001);
-gui.add(variables, 'rarity', 0, 0.3, 0.00001);
-
-var fs = {
-  divide: divide,
-  grow: grow,
-  reset: init_mesh
-};
-
-gui.add(fs, 'grow');
-gui.add(fs, 'divide');
-gui.add(fs, 'reset');
 
 if( !init() )	animate();
 
-function grow() {
-  var max_scale = 0.0012;
 
+function grow(scale) {
   var normalMatrixWorld = new THREE.Matrix3();
+  mesh.geometry.computeVertexNormals();
   var vertices = mesh.geometry.vertices;
   var faces = mesh.geometry.faces;
   var matrixWorld = mesh.matrixWorld;
@@ -42,10 +22,15 @@ function grow() {
     var indices = [face.a, face.b, face.c];
     for (var j = 0; j < 3; j ++) {
       var vv = new THREE.Vector3();
+      var a0 = fv.angleTo(new THREE.Vector3(1, 0, 0));
+      var a1 = fv.angleTo(new THREE.Vector3(0, 1, 0));
+      var a2 = fv.angleTo(new THREE.Vector3(0, 0, 1));
+      var r = Math.floor(Math.random() * (8 - 2)) + 1;
       vertices[ indices[ j ] ].applyMatrix4( matrixWorld );
+
       vertices[ indices[ j ] ].addScaledVector(
         fv,
-        Math.random() < variables.rarity ? (1+Math.sin(Math.random()*2*Math.PI))*variables.scale : 0
+        Math.pow(Math.cos(a0*10*Math.random()), r)*scale*Math.cos(fv.y*Math.random()+fv.x*Math.random()/100.0)*a0
       );
     }
   }
@@ -58,16 +43,35 @@ function divide() {
   mesh.geometry = modifier.modify( mesh.geometry );
 }
 
-function init_mesh() {
-  mesh.geometry = new THREE.SphereGeometry( 0.5, 3, 3);
+function init_geometry() {
+  geometry = new THREE.IcosahedronGeometry(0.5, 2);
+  geometry.dynamic = true;
+  geometry.__dirtyVertices = true;
+}
+
+function reset() {
+  mesh.geometry = new THREE.IcosahedronGeometry(0.5, 2);
+  if(Math.random() > 0.1) grow(Math.random()/10.0);
+  divide();
+  if(Math.random() > 0.3) grow(Math.random()/100.0);
+  divide();
+  if(Math.random() > 0.4) grow(Math.random()/100.0);
+  divide();
+  if(Math.random() > 0.5) grow(Math.random()/100.0);
+  divide();
+
+  mesh.rotateX(2*Math.PI*Math.random());
+  mesh.rotateY(2*Math.PI*Math.random());
+  mesh.rotateZ(2*Math.PI*Math.random());
 }
 
 function init() {
-
   renderer = new THREE.WebGLRenderer({
     antialias: true,
   });
   renderer.setClearColor(0x1e1e1e);
+
+  THREE.MOUSE.ROTATE = 0;
 
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.getElementById('container').appendChild(renderer.domElement);
@@ -75,26 +79,32 @@ function init() {
   scene = new THREE.Scene();
 
   camera	= new THREE.PerspectiveCamera(15, window.innerWidth / window.innerHeight, 1, 10000);
-  camera.position.set(0, 0, 5);
+  camera.position.set(0, 0, 10);
   scene.add(camera);
 
   var light = new THREE.HemisphereLight(0xffffff, 0x888888, 2);
   scene.add(light);
 
-  cameraControls	= new THREE.TrackballControls( camera,  renderer.domElement  )
+  cameraControls	= new THREE.TrackballControls(camera,  renderer.domElement);
 
-  geometry = new THREE.SphereGeometry(0.3, 3, 3);
-  geometry.dynamic = true;
-  geometry.__dirtyVertices = true;
+  init_geometry()
 
   material = new THREE.MeshNormalMaterial();
   mesh = new THREE.Mesh( geometry, material );
   scene.add(mesh);
+
+  //helper = new THREE.FaceNormalsHelper( mesh, 2, 0x00ff00, 1 );
+  //scene.add(helper);
+  //grow(100);
+  //divide();
+
+  reset();
 }
 
 function animate() {
   requestAnimationFrame( animate );
   render();
+  //grow(0.1);
 }
 
 function render() {
